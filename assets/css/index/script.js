484494c1577b53ef0c8cdd3c35762a2378912c1d
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-02-18 18:59:32 -0800

    connected javascript features to Html webpages, Homepage and landing page fixed

--- assets/css/index/script.js
+++ assets/css/index/script.js
@@ -33,21 +33,24 @@
 // });
 // }
 
-var qeury = $("#userInput").val()
-var testLocation = $("#locationInput").val()
-$("#searchButton").on("click", function(){
-    
-    search()
 
+$("#searchButton").on("click", function(){
+    console.log($("#userInput").val())
+    console.log($("#locationInput").val())
+    event.preventDefault()
+    var what = $("#userInput").val()
+    var where = $("#locationInput").val() 
+    localStorage.setItem(what, where)
+    search()
+    searchHistory()
 })
 
 function search(){ // function for retrieving data from APIs
     $.ajax({ // ajax called for pulling Adzuna jobs
         method: "GET",
-        url:"https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=ba503580&app_key=ab0edfb98c4f1c19918f7b4366c2c833&results_per_page=10&what=" + qeury + "&where=" + testLocation,
-        dataType: "json",
+        url:"https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=ba503580&app_key=ab0edfb98c4f1c19918f7b4366c2c833&results_per_page=10&what=" + $("#userInput").val() + "&where=" + $("#locationInput").val(),
         success: function(response){
-            var div = $("#testDiv")
+            var div = $("#searchResults")
             for(var i = 0; i < response.results.length; i++){
                 var job = { // object with all job information
                     company: response.results[i].company.display_name, // company name
@@ -56,7 +59,11 @@ function search(){ // function for retrieving data from APIs
                     description: response.results[i].description // description
                     //url: response.results[i].redirect_url
                 }
-                div.append(JSON.stringify(job))
+
+                div.append( 
+                    "<tr> <td>" + job.company + "</td> <td>" + job.title + "</td> <td>" + job.location + "</td> <td>" + job.description + "</td>  </tr>"
+                )
+                div.append("<a href='" + response.results[i].redirect_url + "'>Apply Now</a>")
             }
             console.log(response)
 
@@ -65,18 +72,50 @@ function search(){ // function for retrieving data from APIs
             console.log(error)
         }
     })
-
+    /*
     $.ajax({ // ajax call for pulling TheMuse jobs
         method: "GET",
-        url:"https://www.themuse.com/api/public/jobs?q=" + qeury + "&location=" + testLocation + "&page=1",
+        url:    "https://www.themuse.com/api/public/jobs?q=" + $("#userInput").val() + "&location=" + $("#locationInput").val() + "&page=1",
         dataType: "json",
         success: function(response){
-
             console.log(response)
+            var div = $("#searchResults") // add scroll wheel here
+            for(var i = 0; i < response.results.length; i++){
+                var job = { // object with all job information
+                    company: response.results[i].company.name, // company name
+                    title: response.results[i].name, // job title
+                    location: response.results[i].locations[0].name, // location
+                    description: response.results[i].description // description
+                    //url: response.results[i].redirect_url
+                }
+                    div.append( 
+                        "<tr> <td>" + job.company + "</td> <td>" + job.title + "</td> <td>" + job.location + "</td> <td>" + job.description + "</td>  </tr>"
+                    )
+                
+            }
 
         },
         error: function(error){
             console.log(error)
         }
     })
+    */
+}
+
+    
+    
+$(document).on("click", "#clearHistory", function(event){
+    event.preventDefault()
+    localStorage.clear()
+    $("#recentSearches").empty()
+})
+
+
+function searchHistory(){
+    var history = $("#recentSearches")
+    $("#recentSearches").empty()
+    for( var i = 0; i < localStorage.length; i++){
+        history.append(localStorage.key(i) + " - ")
+        history.append(localStorage.getItem(localStorage.key(i)) + "<br>")
+    }
 }
